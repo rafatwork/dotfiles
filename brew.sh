e5605186207189efd37e509ed2f7947d6d72d049
Raf Philtjens <raf@rafatwork.be>
2021-12-20 12:36:19 +0100

    brew.sh: Remove the deprecated --with-override-system-vi option

--- brew.sh
+++ brew.sh
@@ -51,7 +51,7 @@ brew install wget
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 brew install grep
 brew install openssh
 brew install screen
