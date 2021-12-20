7abc7834c7a8ee1d60f2e58699cfece28b450913
Raf Philtjens <raf@rafatwork.be>
2021-12-20 11:27:04 +0100

    brew.sh: Add various command-line tools and apps

--- brew.sh
+++ brew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools and apps.
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -10,6 +10,9 @@ brew upgrade
 
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
+
+# Taps
+brew tap franklouwers/homebrew-franklouwers # Belgium e-ID (https://github.com/franklouwers/homebrew-franklouwers)
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -80,22 +83,64 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
+brew install awscli
+brew install cask
+brew install curl
+brew install docker-compose
+brew install exiv2
+brew install ffmpeg
 brew install git
 brew install git-lfs
 brew install gs
+brew install htop
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
+brew install nano
 brew install p7zip
 brew install pigz
 brew install pv
+brew install python
 brew install rename
 brew install rlwrap
 brew install ssh-copy-id
+brew install telnet
 brew install tree
 brew install vbindiff
 brew install zopfli
+
+# Development
+brew install composer
+brew install nvm
+brew install yarn
+
+# Apps
+brew install --cask docker
+brew install --cask eid-be
+brew install --cask eid-be-viewer
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask insomnia
+brew install --cask keepassx
+brew install --cask microsoft-edge
+brew install --cask microsoft-excel
+brew install --cask microsoft-outlook
+brew install --cask microsoft-powerpoint
+brew install --cask microsoft-teams
+brew install --cask microsoft-word
+brew install --cask obsidian
+brew install --cask onedrive
+brew install --cask pdfsam-basic
+brew install --cask phpstorm
+brew install --cask sketchup-pro
+brew install --cask slack
+brew install --cask spotify
+brew install --cask teamviewer
+brew install --cask the-unarchiver
+brew install --cask transmit
+brew install --cask visual-studio-code
+brew install --cask vlc
 
 # Remove outdated versions from the cellar.
 brew cleanup
